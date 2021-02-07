66aba47167f2989f7e1678cd70d4f5ed40b98bd0
ruwanmadusanka <ruwan.16@itfac.mrt.ac.lk>
2021-02-07 23:27:58 +0530

    hooks added

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -1,96 +1,99 @@
-import React from 'react';
-import NameListItem from './NameListItem';
+import React, { useState } from "react";
+import NameListItem from "./NameListItem";
 function NameList() {
-
-    const NameList =
-       [{ id:1,
-           gender: "male",
-        name: {
+  const [NameList, setNameList] = useState([
+    {
+      id: 1,
+      gender: "male",
+      name: {
         title: "mr",
         first: "brad",
-        last: "gibson"
+        last: "gibson",
       },
-        picture: {
+      picture: {
         medium: "https://randomuser.me/api/portraits/med/men/75.jpg",
-        
       },
       phone: "011-962-7516",
-    location: {
+      location: {
         street: "9278 new road",
         city: "kilcoole",
         state: "waterford",
-    },},{ id:2,gender: "female",
-    name: {
-    title: "mr",
-    first: "brad change",
-    last: "gibson"
-  },
-    picture: {
-    medium: "https://randomuser.me/api/portraits/med/men/75.jpg",
-    
-  },
-  phone: "011-962-7516",
-location: {
-    street: "9278 new road",
-    city: "kilcoole",
-    state: "waterford",
-},},
-{ id:3,gender: "male",
-    name: {
-    title: "mr",
-    first: "brad",
-    last: "gibson"
-  },
-    picture: {
-    medium: "https://randomuser.me/api/portraits/med/men/75.jpg",
-    
-  },
-  phone: "011-962-7516",
-location: {
-    street: "9278 new road",
-    city: "kilcoole",
-    state: "waterford",
-},},
-{ id:4,gender: "male",
-    name: {
-    title: "mr",
-    first: "brad",
-    last: "gibson"
-  },
-    picture: {
-    medium: "https://randomuser.me/api/portraits/med/men/75.jpg",
-    
-  },
-  phone: "011-962-7516",
-location: {
-    street: "9278 new road",
-    city: "kilcoole",
-    state: "waterford",
-},}
-];
+      },
+    },
+    {
+      id: 2,
+      gender: "female",
+      name: {
+        title: "mr",
+        first: "brad change",
+        last: "gibson",
+      },
+      picture: {
+        medium: "https://randomuser.me/api/portraits/med/men/75.jpg",
+      },
+      phone: "011-962-7516",
+      location: {
+        street: "9278 new road",
+        city: "kilcoole",
+        state: "waterford",
+      },
+    },
+  ]);
 
-const nameListComponent =()=>{
-    return NameList.map((aname)=>{
-        return(
-            <NameListItem 
-            key={aname.id}
-            gender={aname.gender}
-            name={aname.name.first + ' ' + aname.name.last}
-            avatar={aname.avatar}
-            phoneno={aname.phone}
-            location={aname.location.city}
-            />
-        );
-    })
-}
+  const nameListComponent = () => {
+    return NameList.map((aname) => {
+      return (
+        <NameListItem
+          key={aname.id}
+          gender={aname.gender}
+          name={aname.name.first + " " + aname.name.last}
+          avatar={aname.avatar}
+          phoneno={aname.phone}
+          location={aname.location.city}
+        />
+      );
+    });
+  };
 
+  const addNewUser = () => {
+    const newUser = {
+        id: 5,
+      gender: "male",
+      name: {
+        title: "mr",
+        first: "brad",
+        last: "gibson",
+      },
+      picture: {
+        medium: "https://randomuser.me/api/portraits/med/men/75.jpg",
+      },
+      phone: "011-962-7516",
+      location: {
+        street: "9278 new road",
+        city: "kilcoole",
+        state: "waterford",
+      }
+    };
 
-    return(
-        <div>
-        {nameListComponent()}
-        </div>
-    );
-    
+    // setNameList ((NameList)=>NameList.concat(newUser));
+    setNameList ([...NameList,newUser]);
+  };
+
+  const [num, setNum] = useState(0);
+  const clickButton = () => {
+    console.log("clicked");
+    setNum((num1) => num1 + 2);
+  };
+
+  return (
+    <div>
+        
+      {nameListComponent()}
+      <button onClick={clickButton}>Click me</button>
+      <button onClick={addNewUser}>Add new user</button>
+      {num}
+    </div>
+  );
 }
 
 export default NameList;
